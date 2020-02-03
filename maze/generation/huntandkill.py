deef9354d05199a201a54120a4322be1110bd942
tankalav <52223087+tankalav@users.noreply.github.com>
2020-02-03 13:14:06 -0500

    Created HuntAndKill file

---
+++ maze/generation/huntandkill.py
@@ -0,0 +1,15 @@
+from .mazecreator import MazeCreator
+
+def HuntAndKill(MazeCreator):
+    
+    def __init__(self):
+        pass
+    
+    def prepareGen(self, seed=None):
+        pass
+
+    def step(self):
+        pass
+
+    def complete(self):
+        pass
